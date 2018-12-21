37ea0ac54c03808702c0709fb7baf3fc063bc424
lxgy <45964624+lxgy@users.noreply.github.com>
2018-12-21 10:39:12 +0800

    Delete package-info.java

--- co-college-util-api/src/main/java/net/seehope/college/util/file/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @BelongsProject: co-college
- * @BelongsPackage: net.seehope.college.util.file
- * @Author: lxgy
- * @CreateTime: 2018-12-14 00:34
- * @Description: ${Description}
- */
-package net.seehope.college.util.file;
